56b5de9b28fbbe16c618cc87b33270583676499f
liorBurg <lior.burg@wework.com>
2018-04-15 18:51:30 +0300

    Adding data to restaurants db

--- db/migrate/20180415125540_create_restaurants.rb
+++ db/migrate/20180415125540_create_restaurants.rb
@@ -3,7 +3,7 @@ class CreateRestaurants < ActiveRecord::Migration[5.1]
     create_table :restaurants do |t|
       t.string :name
       t.string :cuisine
-      t.integer :rating
+      t.integer :rating, default: 0
       t.boolean :accepts10bis
       t.string :address
       t.integer :maxDeliveryTime
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Restaurants.csv'))
+csv = CSV.parse(csv_text.scrub, headers: true)
+csv.each do |row|
+  t = Restaurant.new
+  t.name = row[0]
+  t.accepts10bis = row['10bis']
+  t.maxDeliveryTime = row['maxDT']
+  t.cuisine = row['Cuisine']
+  t.address = row['Address']
+  t.save
+end
+
+puts "There are now #{Restaurant.count} rows in the transactions table"
