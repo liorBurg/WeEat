3d24e2f8b04c688db28ad3ab831e46f5e14c70ed
liorBurg <lior.burg@wework.com>
2018-04-26 17:58:28 +0300

    Adding the search restaurant option

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Banner from './Banner';
-import FilterBar from './FilterBar';
 import RestaurantsList from './RestaurantsList';
 import Map from './Map';
 import { getRestaurants } from '../utils/api';
@@ -10,31 +9,44 @@ class App extends Component {
     super(props);
     this.state = {
       rests: [],
+      filteredRests: [],
     };
     this.updateRests = this.updateRests.bind(this);
+    this.filterRests = this.filterRests.bind(this);
   }
   componentDidMount() {
     this.updateRests();
   }
   updateRests() {
     getRestaurants()
-      .then(res => this.setState({ rests: res.data }))
+      .then(res => this.setState({
+        rests: res.data,
+        filteredRests: res.data,
+      }))
       .catch(err => console.log(err));
+  }
+  filterRests(value) {
+    let updatedRests = this.state.rests;
+    updatedRests = updatedRests.filter(function (rest) {
+      return rest.name.toLowerCase().search(value.toLowerCase()) !== -1;
+    });
+    this.setState({ filteredRests: updatedRests });
   }
 
   render() {
     return (
       <div>
-        {this.state.rests.length > 0 ? console.log(this.state.rests) : console.log('empty')}
-        <Banner />
-        <FilterBar />
+        <Banner filterRests={this.filterRests}/>
         <div className="restaurants-container">
-          <RestaurantsList rests={this.state.rests}/>
+          <RestaurantsList rests={this.state.filteredRests}/>
           <Map />
         </div>
       </div>
     );
   }
 }
+
+//        {this.state.rests.length > 0 ? console.log(this.state.rests) : console.log('empty')}
+
 
 export default App;
--- app/javascript/packs/components/Banner.jsx
+++ app/javascript/packs/components/Banner.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import FilterBar from './FilterBar';
+import PropTypes from 'prop-types';
 
 class Banner extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({ searchValue: event.target.value });
+    this.props.filterRests(event.target.value);
+  }
+
   render() {
     return (
       <div className="banner">
-        <h1>WeEat</h1>
+        <div className="header">
+          <h1>WeEat</h1>
+          <input
+            type="text"
+            value={this.state.searchValue}
+            placeholder="Find a restaurant"
+            onChange={this.handleChange}
+          />
+        </div>
+        <FilterBar />
       </div>
     );
   }
 }
+
+Banner.propTypes = {
+  filterRests: PropTypes.func,
+};
 
 export default Banner;
